845f0fb19a6c6aaa5211a6779655945b8898da47
Bryner Bodollo <bsbodollo01@gmail.com>
2022-12-11 21:19:56 +0800

    Fix: modal view for office and search box

--- views/office/OfficeDocumentsTable.tsx
+++ views/office/OfficeDocumentsTable.tsx
@@ -1,60 +1,87 @@
 import moment from "moment";
+import ViewRecord from "../../components/admin/ViewRecord";
+import useCrudModals from "../../hooks/useCrudModals";
+import useModalIDs from "../../hooks/useModalIDs";
 
 const OfficeDocumentsTable = ({ officeDocuments }: any) => {
-	return (
-		<div className="overflow-x-auto">
-			<table className="min-w-max w-full table-auto">
-				<thead>
-					<tr className="bg-gray-100 text-gray-600 text-sm leading-normal">
-						<th className="py-3 px-6 text-left">Case No.</th>
-						<th className="py-3 px-6 text-left">Date Submitted</th>
-						<th className="py-3 px-6 text-left">Status</th>
-						<th className="py-3 px-6 text-center">Actions</th>
-					</tr>
-				</thead>
-				<tbody className="text-gray-600 text-sm">
-					{officeDocuments.map((doc: any) => {
-						return (
-							<tr
-								key={doc.id}
-								className="border-b border-gray-200 hover:bg-gray-50"
-							>
-								<td className="py-3 px-6 text-left whitespace-nowrap">
-									{doc.case__case_no}
-								</td>
-								<td className="py-3 px-6 text-left whitespace-nowrap">
-									{moment(doc.date_received).format("LL")}
-								</td>
-								<td className="py-3 px-6 text-left whitespace-nowrap">
-									{doc.status}
-								</td>
-								<td className="py-3 px-6 text-center">
-									<div className="flex items-center justify-center gap-x-5">
-										<div className="w-4 mr-2 transform hover:text-purple-600 hover:scale-110 cursor-pointer">
-											<svg
-												xmlns="http://www.w3.org/2000/svg"
-												fill="none"
-												viewBox="0 0 24 24"
-												stroke="currentColor"
-												className="w-5 h-5"
-											>
-												<path
-													strokeLinecap="round"
-													strokeLinejoin="round"
-													strokeWidth="2"
-													d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z"
-												/>
-											</svg>
-										</div>
-									</div>
-								</td>
-							</tr>
-						);
-					})}
-				</tbody>
-			</table>
-		</div>
-	);
+    const { viewModal, setViewModal } = useCrudModals();
+    const { selectedObject, setSelectedObject } = useModalIDs();
+
+    const onViewDocument = (docs: any) => {
+        setSelectedObject(docs);
+        setViewModal(true);
+    };
+
+    return (
+        <div className="overflow-x-auto">
+            <ViewRecord
+                isShow={viewModal}
+                viewTitle="Office Documents"
+                viewText="View Office Documents"
+                onClose={() => setViewModal(false)}
+                selectedRecord={selectedObject}
+            />
+            <table className="min-w-max w-full table-auto">
+                <thead>
+                    <tr className="bg-gray-100 text-gray-600 text-sm leading-normal">
+                        <th className="py-3 px-6 text-left">Case No.</th>
+                        <th className="py-3 px-6 text-left">Date Submitted</th>
+                        <th className="py-3 px-6 text-left">Status</th>
+                        <th className="py-3 px-6 text-center">Actions</th>
+                    </tr>
+                </thead>
+                <tbody className="text-gray-600 text-sm">
+                    {officeDocuments.map((doc: any) => {
+                        return (
+                            <tr
+                                key={doc.id}
+                                className="border-b border-gray-200 hover:bg-gray-50"
+                            >
+                                <td className="py-3 px-6 text-left whitespace-nowrap">
+                                    {doc.case__case_no}
+                                </td>
+                                <td className="py-3 px-6 text-left whitespace-nowrap">
+                                    {moment(doc.date_received).format("LL")}
+                                </td>
+                                <td className="py-3 px-6 text-left whitespace-nowrap">
+                                    {doc.status}
+                                </td>
+                                <td className="py-3 px-6 text-center">
+                                    <div className="flex items-center justify-center gap-x-5">
+                                        <div
+                                            className="w-4 mr-2 transform hover:text-purple-600 hover:scale-110 cursor-pointer"
+                                            onClick={() => onViewDocument(doc)}
+                                        >
+                                            <svg
+                                                xmlns="http://www.w3.org/2000/svg"
+                                                fill="none"
+                                                viewBox="0 0 24 24"
+                                                stroke="currentColor"
+                                                className="w-5 h-5"
+                                            >
+                                                <path
+                                                    strokeLinecap="round"
+                                                    strokeLinejoin="round"
+                                                    strokeWidth="2"
+                                                    d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+                                                />
+                                                <path
+                                                    strokeLinecap="round"
+                                                    strokeLinejoin="round"
+                                                    strokeWidth="2"
+                                                    d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"
+                                                />
+                                            </svg>
+                                        </div>
+                                    </div>
+                                </td>
+                            </tr>
+                        );
+                    })}
+                </tbody>
+            </table>
+        </div>
+    );
 };
 
 export default OfficeDocumentsTable;
--- views/office/OfficeView.tsx
+++ views/office/OfficeView.tsx
@@ -2,73 +2,74 @@ import { useCallback, useState, useEffect } from "react";
 import { useForm } from "react-hook-form";
 import ImageUploader from "../../components/ImageUploader";
 import { useAppDispatch, useAppSelector } from "../../redux/hooks";
-import { getOfficeDocuments } from "../../redux/dataSlice";
+import { getOfficeDocuments, updateDocument } from "../../redux/dataSlice";
 import { fieldRules } from "../../components/authHelper";
 import InputWithSubmit from "../../components/InputWithSubmit";
 import MoonLoader from "react-spinners/MoonLoader";
 import OfficeDocumentsTable from "./OfficeDocumentsTable";
 
 const OfficeView = () => {
-	const dispatch = useAppDispatch();
-	const { dataLoading, officeDocuments } = useAppSelector(
-		(state) => state.dataState
-	);
-	const { userProfile } = useAppSelector((state) => state.authState);
+    const dispatch = useAppDispatch();
+    const { dataLoading, officeDocuments } = useAppSelector(
+        (state) => state.dataState
+    );
+    const { userProfile } = useAppSelector((state) => state.authState);
 
-	const { handleSubmit, control } = useForm();
+    const { handleSubmit, control } = useForm();
 
-	useEffect(() => {
-		dispatch(getOfficeDocuments());
-	}, []);
+    useEffect(() => {
+        dispatch(getOfficeDocuments());
+    }, []);
 
-	const onSubmitQrTracker = (formData: any) => {
-		console.log(formData);
-	};
+    const onSubmitQrTracker = (formData: any) => {
+        console.log(formData.officeQrTracker);
+        dispatch(updateDocument(formData));
+    };
 
-	return (
-		<div className="min-h-screen flex flex-col items-center gap-y-20 font-mont text-gray-700">
-			<div className="w-[80%] bg-white font-mont flex flex-col gap-y-5 text-gray-700 p-5 shadow border-b border-gray-200 rounded-lg mt-10">
-				<h4 className="text-xl font-black tracking-wider">
-					{userProfile.first_name}
-				</h4>
-				<div className="w-full border-b border-gray-200 -mt-3"></div>
-				<div className="w-full flex flex-col items-center gap-y-5 py-5">
-					<div className="flex items-center gap-x-5">
-						<div className="w-20 border-b border-gray-400"></div>
-						<p className="text-sm">Enter QR Code Tracker</p>
-						<div className="w-20 border-b border-gray-400"></div>
-					</div>
-					<InputWithSubmit
-						control={control}
-						fieldType="text"
-						fieldName="officeQrTracker"
-						fieldRules={fieldRules.requiredRule}
-						placeHolder="QR Code Tracker"
-						onClickSubmit={handleSubmit(onSubmitQrTracker)}
-					/>
-				</div>
-			</div>
-			<div className="w-[80%] bg-white font-mont flex flex-col gap-y-5 text-gray-700 p-5 shadow border-b border-gray-200 rounded-lg">
-				<h4 className="text-xl font-black tracking-wider">
-					Transfered Documents
-				</h4>
-				<div className="w-full border-b border-gray-200 -mt-3"></div>
-				{dataLoading && (
-					<div className="w-full flex justify-center items-center">
-						<MoonLoader
-							loading={dataLoading}
-							color="#9333ea"
-							speedMultiplier={1}
-							size={70}
-						/>
-					</div>
-				)}
-				{!dataLoading && (
-					<OfficeDocumentsTable officeDocuments={officeDocuments} />
-				)}
-			</div>
-		</div>
-	);
+    return (
+        <div className="min-h-screen flex flex-col items-center gap-y-20 font-mont text-gray-700">
+            <div className="w-[80%] bg-white font-mont flex flex-col gap-y-5 text-gray-700 p-5 shadow border-b border-gray-200 rounded-lg mt-10">
+                <h4 className="text-xl font-black tracking-wider">
+                    {userProfile.first_name}
+                </h4>
+                <div className="w-full border-b border-gray-200 -mt-3"></div>
+                <div className="w-full flex flex-col items-center gap-y-5 py-5">
+                    <div className="flex items-center gap-x-5">
+                        <div className="w-20 border-b border-gray-400"></div>
+                        <p className="text-sm">Enter QR Code Tracker</p>
+                        <div className="w-20 border-b border-gray-400"></div>
+                    </div>
+                    <InputWithSubmit
+                        control={control}
+                        fieldType="text"
+                        fieldName="officeQrTracker"
+                        fieldRules={fieldRules.requiredRule}
+                        placeHolder="QR Code Tracker"
+                        onClickSubmit={handleSubmit(onSubmitQrTracker)}
+                    />
+                </div>
+            </div>
+            <div className="w-[80%] bg-white font-mont flex flex-col gap-y-5 text-gray-700 p-5 shadow border-b border-gray-200 rounded-lg">
+                <h4 className="text-xl font-black tracking-wider">
+                    Transfered Documents
+                </h4>
+                <div className="w-full border-b border-gray-200 -mt-3"></div>
+                {dataLoading && (
+                    <div className="w-full flex justify-center items-center">
+                        <MoonLoader
+                            loading={dataLoading}
+                            color="#9333ea"
+                            speedMultiplier={1}
+                            size={70}
+                        />
+                    </div>
+                )}
+                {!dataLoading && (
+                    <OfficeDocumentsTable officeDocuments={officeDocuments} />
+                )}
+            </div>
+        </div>
+    );
 };
 
 export default OfficeView;
